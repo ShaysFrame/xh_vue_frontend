a47ba2e897213d3b36611bb78b354ecd5cf14ecd
ShaysFrame <akashshaykh@gmail.comm>
2023-04-15 04:42:22 +0800

    deploying to railway

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/'
+axios.defaults.baseURL = 'http://xuehanyu.up.railway.app/api/v1/'
 
 createApp(App).use(store).use(router, axios).mount('#app')
